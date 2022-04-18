d5c84e60dd6f2f119c553b645a1964b0f1efef93
SashaBilchuk <73472739+SashaBilchuk@users.noreply.github.com>
2022-04-18 19:01:59 +0300

    Add files via upload
    Original Emilia 14.4.22 version

---
+++ shelter/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Dog
+from .models import Cat
+# from .models import Foster
+
+# Register your models here.
+admin.site.register(Dog)
+admin.site.register(Cat)
+# admin.site.register(Foster)
+
+

8faa1a868180840ea6715a6dcb5b142247c57e7c
EmiliaKogan <emilya.k93@gmail.com>
2022-04-21 21:06:59 +0300

    שינויים של 21.4.22

--- shelter/models.py
+++ shelter/models.py
@@ -22,26 +22,27 @@ PLACES = (
 
 class Dog(models.Model):
     id = models.AutoField(primary_key=True)
-    chip_number = models.IntegerField(blank=True, verbose_name=_('מספר שבב'))
+    chip_number = models.IntegerField(blank=True, default=None, null=True, verbose_name=_('מספר שבב'))
     name = models.CharField(max_length=255, verbose_name=_('שם'))
     birth_date = models.DateField(default=datetime.date.today, verbose_name=_('תאריך לידה'))
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=6, null = False, verbose_name=_('מין'))
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=6, verbose_name=_('מין'))
     image = models.ImageField(upload_to='mediaDogs/', default='media/generic_img.png', blank=True, verbose_name=_('הוסף תמונה'))
     size = models.CharField(choices=SIZE_CHOICES, max_length=255, verbose_name=_('גודל'))
     color = models.CharField(max_length=255, verbose_name=_('צבע'))
-    description = models.TextField(max_length=255, blank=True, default=None,null = True, verbose_name=_('תיאור הכלב'))
+    description = models.TextField(max_length=255, blank=True, default=None, null = True, verbose_name=_('תיאור הכלב'))
+    behaviour_description = models.TextField(max_length=255, blank=True, default=None, null=True,verbose_name=_('תיאור התנהגותי'))
     location = models.CharField(choices=PLACES, max_length=20, verbose_name=_('מיקום הכלב'))
-    behaviour_description = models.TextField(max_length=255, blank=True, default=None,null = True, verbose_name=_('תיאור התנהגותי'))
     acceptance_date = models.DateField(default=datetime.date.today, verbose_name=_('תאריך קבלה לעמותה'))
-    story = models.TextField(max_length=255, blank=True, default=None,null = True, verbose_name=_('סיפור הכלב'))
-    clinic = models.TextField(max_length=255, blank=True, default=None,null = True, verbose_name=_('מרפאה וטרינרית'))
+    exit_date = models.DateField(default=datetime.date.today, verbose_name=_('תאריך יציאה מעמותה'))
+    story = models.TextField(max_length=255, blank=True, default=None, null=True, verbose_name=_('סיפור הכלב'))
+    clinic = models.TextField(max_length=255, blank=True, default=None, null=True, verbose_name=_('מרפאה וטרינרית'))
     sterilization = models.DateField(blank=True, null=True, verbose_name=_('סירוס/עיקור'))
-    sterilization_approval_link = models.TextField(max_length=255, blank=True, default=None, null=True,verbose_name=_('קישור לאישור סירוס/עיקור'))
+    sterilization_approval = models.ImageField(upload_to='mediaDogs/', blank=True, default=None, verbose_name=_('אישור סירוס/עיקור'))
     notebook = models.BooleanField(default=False, verbose_name=_('יש פנקס חיסונים?'))
     vaccine_book_url = models.TextField(max_length=255, blank=True, default=None, null=True,verbose_name=_('קישור לפנקס חיסונים'))
-    rabbies_vaccine = models.DateField(blank=True, null = True, verbose_name=_('חיסון כלבת'))
-    next_treatment_rabbies = models.DateField(blank=True, null=True, verbose_name=_('כלבת - תאריך הטיפול הבא'))
-    rabbies_vaccine_aproval_link = models.TextField(max_length=255, blank=True, default=None, null=True,verbose_name=_('קישור לאישור חיסון כלבת'))
+    rabies_vaccine = models.DateField(blank=True, null = True, verbose_name=_('חיסון כלבת'))
+    next_treatment_rabies = models.DateField(blank=True, null=True, verbose_name=_('כלבת - תאריך הטיפול הבא'))
+    rabies_vaccine_approval = models.ImageField(upload_to='mediaDogs/', null=False, default=None, verbose_name=_('אישור חיסון כלבת'))
     hexagonal_vaccine = models.DateField(blank=True, null = True, verbose_name=_('חיסון משושה'))
     next_treatment_hexagonal = models.DateField(blank=True, null = True, verbose_name=_('משושה - תאריך הטיפול הבא'))
     nonagonal_vaccine = models.DateField(blank=True, null = True, verbose_name=_('חיסון מתושע'))
@@ -57,8 +58,8 @@ class Dog(models.Model):
     died = models.BooleanField(default=False, verbose_name=_('נפטר/ה'))
     death_date = models.DateField(blank=True, null=True, verbose_name=_('תאריך הפטירה'))
     death_reason = models.TextField(max_length=255, blank=True, default=None, null=True, verbose_name=_('סיבת פטירה'))
-    adopter_name = models.TextField(max_length=255, blank=True, default=None, null=True, verbose_name=_('שם המאמצ/ת'))
     adopter_id = models.IntegerField(default=None, blank=True, null=True, verbose_name=_('ת.ז. של המאמצ/ת'))
+    adopter_name = models.TextField(max_length=255, blank=True, default=None, null=True, verbose_name=_('שם המאמצ/ת'))
     adoption_date = models.DateField(blank=True, null = True, verbose_name=_('תאריך אימוץ'))
     waiver_forms = models.TextField(max_length=255, blank=True, default=None, null=True, verbose_name=_('מסמך העברה'))
     returner_id = models.IntegerField(default=0, blank=True, null=True, verbose_name=_('ת.ז. של מחזיר הכלב/ה'))
@@ -83,7 +84,7 @@ class Dog(models.Model):
 
     @property
     def days_in_the_association(self):
-        if self.acceptance_date is not None:
+        if self.acceptance_date is not None and self.location == 'Association':
             return datetime.date.today().day - self.acceptance_date.day
         else:
             return 0
@@ -101,10 +102,9 @@ class Cat(models.Model):
     description = models.TextField(max_length=360, verbose_name=_('תיאור החתול/ה'))
     location = models.CharField(choices=PLACES, max_length=20, verbose_name=_('מיקום'))
     acceptance_date = models.DateField(default=datetime.date.today, verbose_name=_('תאריך קבלה לעמותה'))
-    number_of_days_in_the_association = models.IntegerField(default=1, verbose_name=_('מספר ימים בעמותה'))
     story = models.TextField(max_length=255, blank=True, default=None, null=True, verbose_name=_('סיפור החתול/ה'))
     sterilization = models.DateField(default=None, verbose_name=_('סירוס/עיקור'))
-    sterilization_approval_link = models.TextField(max_length=255, blank=True, default=None, null=True, verbose_name=_('קישור לאישור סירוס/עיקור'))
+    sterilization_approval = models.ImageField(upload_to='mediaCats/', blank=True, default=None, verbose_name=_('אישור סירוס/עיקור'))
     notebook = models.BooleanField(default=False, verbose_name=_('פנקס חיסונים'))
     vaccine_book_url = models.TextField(max_length=255, blank=True, default=None, null=True, verbose_name=_('קישור לפנקס חיסונים'))
     square_vaccine = models.BooleanField(default=False, verbose_name=_('חיסון מרובע'))
